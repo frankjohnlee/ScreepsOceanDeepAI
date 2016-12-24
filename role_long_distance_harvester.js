da739b53a459573df79a0e08ab33d06545eaac9c
Frank John Lee <frankjohnlee@live.com>
2016-12-24 17:06:32 -0500

    Distance Harvester Builds and Repairs Roads
    Now distance harvesters build roads in their target_rooms and maintains
    them

--- role_long_distance_harvester.js
+++ role_long_distance_harvester.js
@@ -4,7 +4,7 @@ module.exports = {
         //console.log(creep.memory.target_room);
         //console.log(creep.memory.home_room);
 
-        require('function_working_status').run(creep);
+        require('function_working_status').run(creep); // When creep is carrying max energy working becomes false.
         creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD);
 
         var home_room = creep.memory.home_room;
@@ -14,20 +14,45 @@ module.exports = {
         var in_home_room = creep_current_room == home_room;
 
 
-        if (in_target_room && creep.memory.working == false) {
-            creep.say("LDHGetEnergy");
-            var source = require('function_find_closest_source').run(creep);
-                require('function_move_in_range_harvest').run(creep, source);
-            }
-        else if (in_target_room && creep.memory.working == true){
-            require('function_go_to_room').run(creep, creep.memory.target_room);
-            }
-        else if (in_home_room && creep.memory.working == true) {
-            require("role_harvester").run(creep);
-            }
+        // CASE 1: CREEP IS AT HOME WITH NO ENERGY => Creep should go to target_room
         if (in_home_room && creep.memory.working == false) {
-            require('function_go_to_room').run(creep, creep.memory.target_room);
-            }
+            require('function_go_to_target_room').run(creep);
         }
 
+        // CASE 1: CREEP IS AT TARGET ROOM AND DOES HAVE FULL energy => Creep should get energy
+        else if (in_target_room && creep.memory.working == false) {
+            creep.say("LDHGetEnergy");
+            var source = require('function_find_closest_source').run(creep);
+            require('function_move_in_range_harvest').run(creep, source);
+        }
+
+        // CASE 2: CREEP IS FULL OF ENERGY => GO HOME
+        else if (in_target_room && creep.memory.working == true) {
+            var con_site_rest = creep.room.find(FIND_CONSTRUCTION_SITES);
+            var repairSite = creep.room.find(FIND_STRUCTURES, {
+                filter: object => object.hits < 1500 &&
+                (object.structureType != STRUCTURE_WALL && object.structureType != STRUCTURE_RAMPART)
+            });
+            // IF THERE ARE CONSTRUCTION SITES THEN BUILD IT
+            if (con_site_rest) {
+                require('role_builder').run(creep);
+            }
+            // IF THERE ARE ANY ROADS ABOUT TO BREAK DOWN REPAIR IT
+            else if (repairSite) {
+                require('role_repairer').run(creep);
+            }
+            // IF NONE OF THOSE ARE PRESSING ISSUES THEN GO HOME AND DELIVER ENERGY
+            else {
+                require('function_go_to_home_room').run(creep);
+            }
+
+        }
+
+        // CASE 3: CREEP IS HOME AND FULL OF ENERGY => BECOME HARVESTER
+        if (in_home_room && creep.memory.working == true) {
+            require("role_harvester").run(creep);
+        }
+
+
+    }
 };
