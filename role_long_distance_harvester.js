b2651f8ea2defbe9eb3f075dea73a9a7739826cc
Frank John Lee <frankjohnlee@live.com>
2016-12-25 14:48:09 -0500

    LD harvester, repair first before building
    Just so you don't end have broken roads while attempting to build roads

--- role_long_distance_harvester.js
+++ role_long_distance_harvester.js
@@ -27,16 +27,16 @@ module.exports = {
         else if (in_target_room && creep.memory.working == true) {
             var con_site_rest = creep.room.find(FIND_CONSTRUCTION_SITES);
             var repairSite = creep.room.find(FIND_STRUCTURES, {
-                filter: object => object.hits < 1500 &&
+                filter: object => object.hits < 1000 &&
                 (object.structureType != STRUCTURE_WALL && object.structureType != STRUCTURE_RAMPART)
             });
             // IF THERE ARE CONSTRUCTION SITES THEN BUILD IT
             if (con_site_rest) {
-                require('role_builder').run(creep);
+                require('role_repairer').run(creep);
             }
             // IF THERE ARE ANY ROADS ABOUT TO BREAK DOWN REPAIR IT
             else if (repairSite) {
-                require('role_repairer').run(creep);
+                require('role_builder').run(creep);
             }
             // IF NONE OF THOSE ARE PRESSING ISSUES THEN GO HOME AND DELIVER ENERGY
             else {
